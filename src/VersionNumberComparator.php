48a2169810af487e7da0466627a83b7ba81296c3
Javi H. Gil <javihgil@softspring.es>
2024-04-25 10:00:29 +0200

    Add rector

--- src/VersionNumberComparator.php
+++ src/VersionNumberComparator.php
@@ -4,14 +4,16 @@ namespace Softspring\Component\DoctrineMigrationsVersionComparator;
 
 use Doctrine\Migrations\Version\Comparator;
 use Doctrine\Migrations\Version\Version;
+use ReflectionClass;
+use ReflectionException;
 
 class VersionNumberComparator implements Comparator
 {
     public function compare(Version $a, Version $b): int
     {
         try {
-            return (new \ReflectionClass("$a"))->getShortName() <=> (new \ReflectionClass("$b"))->getShortName();
-        } catch (\ReflectionException $e) {
+            return (new ReflectionClass("$a"))->getShortName() <=> (new ReflectionClass("$b"))->getShortName();
+        } catch (ReflectionException $e) {
             return 0;
         }
     }
